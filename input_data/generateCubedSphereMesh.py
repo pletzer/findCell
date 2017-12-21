055bc081fd64ddc8bfc41c367398cf4c28e54278
pletzer <alexander@gokliya.net>
2017-12-21 02:15:19 +0000

    + generate sets of random points

--- input_data/generateCubedSphereMesh.py
+++ input_data/generateCubedSphereMesh.py
@@ -4,10 +4,12 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate cubed sphere grid')
 parser.add_argument('-n', type=int, default=10, help='Number of cells along each direction of the tiles')
 parser.add_argument('-z', type=int, default=2, help='Number of vertical cells')
+parser.add_argument('-a', type=float, default=1.0, help='Radius')
+parser.add_argument('-r', type=float, default=0.1, help='Max relative elevation')
 args = parser.parse_args()
 
 numCells = args.n
 numElvs = args.z
 
-cs = CubedSphereElv(numCells, numElvs=numElvs)
+cs = CubedSphereElv(numCells, numElvs=numElvs, radius=args.a, maxRelElv=args.r)
 cs.save('cs.vtk')
---
+++ input_data/generateTargetPoints.py
@@ -0,0 +1,43 @@
+import random
+import argparse
+import numpy
+
+parser = argparse.ArgumentParser(description='Generate random points')
+parser.add_argument('-N', type=int, default=100, help='Number of points')
+parser.add_argument('-a', type=float, default=1.0, help='Radius')
+parser.add_argument('-r', type=float, default=0.1, help='Max relative elevation')
+args = parser.parse_args()
+
+n = args.N
+maxRelElv = args.r
+radius = args.a
+
+numpy.random.seed(1234)
+lams = numpy.random.rand(n) * 2*numpy.pi
+thes = -numpy.pi/2. + numpy.pi*numpy.random.rand(n)
+elvs = radius*(maxRelElv*numpy.random.rand(n) + 1.0)
+xyz = numpy.zeros((n, 3), numpy.float64)
+xyz[:, 0] = elvs*numpy.cos(thes)*numpy.cos(lams)
+xyz[:, 1] = elvs*numpy.cos(thes)*numpy.cos(lams)
+xyz[:, 2] = elvs*numpy.sin(thes)
+
+# save the points as a set of x,y,z points
+import vtk
+mesh = vtk.vtkUnstructuredGrid()
+pts = vtk.vtkPoints()
+ptdata = vtk.vtkDoubleArray()
+
+ptdata.SetNumberOfTuples(n)
+ptdata.SetNumberOfComponents(3) # 3d
+ptdata.SetVoidArray(xyz, 3*n, 1)
+
+pts.SetNumberOfPoints(n)
+pts.SetData(ptdata)
+
+mesh.SetPoints(pts)
+#no cell
+
+writer = vtk.vtkUnstructuredGridWriter()
+writer.SetFileName('points.vtk')
+writer.SetInputData(mesh)
+writer.Update()
