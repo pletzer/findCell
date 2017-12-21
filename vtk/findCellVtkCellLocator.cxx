e9f92a682ab6868d80eab053638b5bd03cd7989f
pletzer <alexander@gokliya.net>
2017-12-21 08:37:47 +0000

    + messages

--- vtk/findCellVtkCellLocator.cxx
+++ vtk/findCellVtkCellLocator.cxx
@@ -6,6 +6,8 @@
 #include <CmdLineArgParser.h>
 #include <vtkUnstructuredGridReader.h>
 #include <vtkUnstructuredGrid.h>
+#include <vtkCellLocator.h>
+#include <vtkGenericCell.h>
 
 int main(int argc, char** argv) {
 
@@ -14,6 +16,7 @@ int main(int argc, char** argv) {
     args.set("-m", std::string("cs.vtk"), "Mesh file name.");
     args.set("-p", std::string("points.vtk"), "File name containing target points.");
     args.set("-t", 1.e-10, "Tolerance.");
+    args.set("-b", 100, "Average number of cells per bucket in cell locator.");
     bool success = args.parse(argc, argv);
     bool help = args.get<bool>("-h");
 
@@ -41,10 +44,16 @@ int main(int argc, char** argv) {
     vtkUnstructuredGrid* points = pointReader->GetOutput();
     std::cout << "Point file: " << args.get<std::string>("-p") << " no points: " << points->GetNumberOfPoints() << '\n';
 
+    // build the cell locator
+    vtkCellLocator* loc = vtkCellLocator::New();
+    loc->SetDataSet(points);
+    loc->SetNumberOfCellsPerBucket(args.get<int>("-b"));
+    loc->BuildLocator();
+
     // iterate over each point
     double xyz[3];
     const double tol2 = args.get<double>("-t");
-    int subId;
+    vtkGenericCell* genCell = vtkGenericCell::New();
     double pcoords[3];
     double weights[8];
     int totFailures = 0;
@@ -52,7 +61,7 @@ int main(int argc, char** argv) {
     std::clock_t tic = clock();
     for (size_t i = 0; i < numPoints; ++i) {
         points->GetPoints()->GetPoint(i, xyz);
-        vtkIdType cellId = mesh->FindCell(xyz, NULL, 0, tol2, subId, pcoords, weights);
+        vtkIdType cellId = loc->FindCell(xyz, tol2, genCell, pcoords, weights);
         if (cellId < 0) totFailures++;
     }
     std::clock_t toc = clock();
@@ -62,8 +71,10 @@ int main(int argc, char** argv) {
     std::cout << "Elapsed time (s)  : " << elapsed_secs << '\n';
 
     // clean up
+    genCell->Delete();
+    loc->Delete();
     pointReader->Delete();
     meshReader->Delete();
-    
+
     return 0;
 }
